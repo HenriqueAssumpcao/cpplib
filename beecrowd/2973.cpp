4a974de7a413fcfe5b320f73dba5c1bb61bbdf3f
HenriqueAssumpcao <henriquesas2010@gmail.com>
2025-03-28 23:17:37 -0300

    ...

--- beecrowd/2973.cpp
+++ beecrowd/2973.cpp
@@ -9,83 +9,55 @@ using namespace std;
 
 typedef long long ll;
 
-template <typename T>
-void print_vector(vector<T> &vec){
-    for(unsigned i = 0; i < vec.size(); i++){
-        cout << vec[i] << " "; 
-    }
-    cout << endl;
-}
 
-template <typename T>
-void print_matrix(vector<vector<T>> &M){
-    for(unsigned i = 0; i < M.size(); i++){
-        print_vector<T>(M[i]);
-    }
-}
-
-
-int sol_t(int t,vector<int> &times, int c){
-    // cout << "here!" << endl;
-    // cout << "c = " << c << endl;
-    vector<int> times_per_subarray(c,0);
-
+bool sol_t(int p,vector<int> &buckets, int c){
+    vector<int> buckets_per_subarray(c,0);
 
     int curr_arr = 0;
     int right = 0;
-    int max = -1;
-    
-    // cout << "here!!" << endl;
 
-    while(curr_arr < c){
-        if(times_per_subarray[curr_arr] + times[right] <= t){
-            times_per_subarray[curr_arr] += times[right];
+    while(curr_arr < c && right < buckets.size()){
+        if(buckets_per_subarray[curr_arr] + buckets[right] <= p){
+            buckets_per_subarray[curr_arr] += buckets[right];
             right++;
-            if(times_per_subarray[curr_arr] > max){
-                max = times_per_subarray[curr_arr];
-            }
         }
         else{
             curr_arr++;
         }
     }
-    if(right < times.size()){
-        max = -1;
-    }
 
-    // print_vector<int>(times_per_subarray);
-    return max;
+    return right == buckets.size();
 }
 
-int bin_search_sol(int left, int right, vector<int> &times, int &c){
-    cout << "left = " << left << ";right = " << right << endl;
+int bin_search_sol(int left, int right, vector<int> &buckets, int &c,int &t){
     if(left == right){
-        return sol_t(left,times,c);
-    }
-    else if(right-left == 1){
-        int ret_left = sol_t(left,times,c);
-        int ret_right = sol_t(right,times,c);
-        if(ret_right == -1){
-            return ret_right;
+        if(sol_t(left*t,buckets,c)){
+            return left;
         }
         else{
-            if(ret_left == -1){
-                return ret_right;
+            return -1;
+        }
+    }
+    else if(right-left == 1){
+        if(!sol_t(right*t,buckets,c)){
+            return -1;
+        }
+        else{
+            if(!sol_t(left*t,buckets,c)){
+                return right;
             }
             else{
-                return ret_left;
+                return left;
             }
         }
     }
     int middle = left + (right-left+1)/2;
-    cout << "middle = " << middle << endl;
-    cout << sol_t(middle,times,c) << endl;
 
-    if(sol_t(middle,times,c) == -1){
-        return bin_search_sol(middle+1,right,times,c);
+    if(!sol_t(middle*t,buckets,c)){
+        return bin_search_sol(middle+1,right,buckets,c,t);
     }
     else{
-        return bin_search_sol(left,middle,times,c);
+        return bin_search_sol(left,middle,buckets,c,t);
     }
 }
 
@@ -93,29 +65,23 @@ int bin_search_sol(int left, int right, vector<int> &times, int &c){
 
 
 int main(){
-    //std::ios::sync_with_stdio(0);cin.tie(0);
+    std::ios::sync_with_stdio(0);cin.tie(0);
 
     int n,c,t;
     cin >> n >> c >> t;
 
     c = min(c,n);
 
-    // vector<int> buckets(n);
-    vector<int> times(n);
+    vector<int> buckets(n);
     
-    int temp;
     for(int i = 0; i < n; i++){
-        cin >> temp;
-        times[i] = 1 + ((temp-1)/t);
+        cin >> buckets[i];
     }
-    cout << n << " " << c << " " << t << endl;
 
-    print_vector<int>(times);
+    int min_t = (*max_element(buckets.begin(),buckets.end()) - 1)/t;
+    int max_t = ((accumulate(buckets.begin(),buckets.end(),0) - 1)/t)+1;
 
-    int min_t = *max_element(times.begin(),times.end());
-    int max_t = accumulate(times.begin(),times.end(),0);
-
-    cout << bin_search_sol(min_t,max_t,times,c) << endl;
+    cout << bin_search_sol(min_t,max_t,buckets,c,t) << endl;
 
 
     return 0;
