935b36f8fe958c1262b7becd89b4ed13edba279b
HenriqueAssumpcao <henriquesas2010@gmail.com>
2025-04-24 18:58:59 -0300

    added new solution

--- problems/beecrowd/2778.cpp
+++ problems/beecrowd/2778.cpp
@@ -8,52 +8,125 @@ using namespace std;
 #define IINF INT_MAX/2
 
 typedef long long ll;
-typedef vector<list<pair<int,ll>>> wgraph; // weighted graph
-typedef vector<list<int>> graph; // unweighted graph
 
 typedef vector<vector<int>> imatrix;
 typedef vector<vector<ll>> llmatrix;
 typedef vector<vector<vector<vector<int>>>> imatrix4d;
+typedef vector<vector<vector<vector<pair<int,int>>>>> pmatrix4d;
 
-template <typename T>
-void print_vector(vector<T> &vec){
-    for(unsigned i = 0; i < vec.size(); i++){
-        cout << vec[i] << " "; 
+
+void sol_dp(int n, int m, int x, int y, imatrix &M, imatrix4d &dp){
+    dp = imatrix4d(n,vector<imatrix>(m,imatrix(x+1,vector<int>(y+1,IINF))));
+    pmatrix4d parent(n,vector<vector<vector<pair<int,int>>>>(m,vector<vector<pair<int,int>>>(x+1,vector<pair<int,int>>(y+1,{-1,-1}))));
+
+    for(int xx = 0; xx <= x; xx++){
+        for(int yy = 0; yy <= y; yy++){
+            if(M[0][0] > 0 || (M[0][0] == 0 && xx > 0) || (M[0][0] < 0 && yy > 0)){
+                dp[0][0][xx][yy] = M[0][0];
+            }
+        }
     }
-    cout << endl;
-}
 
-template <typename T>
-void print_matrix(vector<vector<T>> &M){
-    for(unsigned i = 0; i < M.size(); i++){
-        print_vector<T>(M[i]);
+    for(int xx = 0;xx <= x; xx++){
+        for(int yy = 0;yy <= y; yy++){
+            for(int j = 1; j < m; j++){
+                if(M[0][j] > 0 && dp[0][j-1][xx][yy] != IINF){
+                    dp[0][j][xx][yy] = M[0][j] + dp[0][j-1][xx][yy];
+                    parent[0][j][xx][yy] = {0,j-1};
+                }
+                else if(M[0][j] == 0 && xx > 0 && dp[0][j-1][xx-1][yy] != IINF){
+                    dp[0][j][xx][yy] = M[0][j] + dp[0][j-1][xx-1][yy];
+                    parent[0][j][xx][yy] = {0,j-1};   
+                }
+                else if(M[0][j] < 0 && yy > 0 && dp[0][j-1][xx][yy-1] != IINF){
+                    dp[0][j][xx][yy] = M[0][j] + dp[0][j-1][xx][yy-1];
+                    parent[0][j][xx][yy] = {0,j-1}; 
+                }
+            }
+        }
+    }
+
+    for(int i = 1; i < n; i++){
+        for(int xx = 0; xx <= x; xx++){
+            for(int yy = 0; yy <= y; yy++){
+                // compute first using left and up neighbors
+                for(int j = 0; j < m; j++){
+                    if(M[i][j] > 0){
+                        if(dp[i-1][j][xx][yy] != IINF){
+                            dp[i][j][xx][yy] = dp[i-1][j][xx][yy] + M[i][j];
+                            parent[i][j][xx][yy] = {i-1,j};
+                        }
+                        if((j > 0) && (dp[i][j-1][xx][yy] != IINF)){
+                            if(M[i][j] + dp[i][j-1][xx][yy] < dp[i][j][xx][yy]){
+                                dp[i][j][xx][yy] = M[i][j] + dp[i][j-1][xx][yy];
+                                parent[i][j][xx][yy] = {i,j-1};
+                            }
+                        }
+                    }
+                    else if(M[i][j] == 0 && xx > 0){
+                        if(dp[i-1][j][xx-1][yy] != IINF){
+                            dp[i][j][xx][yy] = dp[i-1][j][xx-1][yy] + M[i][j];
+                            parent[i][j][xx][yy] = {i-1,j};
+                        }
+                        if((j > 0) && (dp[i][j-1][xx-1][yy] != IINF) && (parent[i][j-1][xx-1][yy].first != i || parent[i][j-1][xx-1][yy].second != j)){
+                            if(M[i][j] + dp[i][j-1][xx-1][yy] < dp[i][j][xx][yy]){
+                                dp[i][j][xx][yy] = M[i][j] + dp[i][j-1][xx-1][yy];
+                                parent[i][j][xx][yy] = {i,j-1};
+                            }
+                        }
+                    }
+                    else{
+                        if(yy > 0){
+                            if(dp[i-1][j][xx][yy-1] != IINF){
+                                dp[i][j][xx][yy] = dp[i-1][j][xx][yy-1] + M[i][j];
+                                parent[i][j][xx][yy] = {i-1,j};
+                            }
+                            if((j > 0) && (dp[i][j-1][xx][yy-1] != IINF) && (parent[i][j-1][xx][yy-1].first != i || parent[i][j-1][xx][yy-1].second != j)){
+                                if(M[i][j] + dp[i][j-1][xx][yy-1] < dp[i][j][xx][yy]){
+                                    dp[i][j][xx][yy] = M[i][j] + dp[i][j-1][xx][yy-1];
+                                    parent[i][j][xx][yy] = {i,j-1};
+                                }
+                            }
+                        }
+                        else{
+                            dp[i][j][xx][yy] = IINF;
+                        }
+                    }
+                }
+                // compare with possible right paths
+                for(int j = m-2; j >= 0; j--){
+                    if(M[i][j] > 0){
+                        if((dp[i][j+1][xx][yy] != IINF) && (parent[i][j+1][xx][yy].first != i || parent[i][j+1][xx][yy].second != j)){
+                            if(M[i][j] + dp[i][j+1][xx][yy] < dp[i][j][xx][yy]){
+                                dp[i][j][xx][yy] = M[i][j] + dp[i][j+1][xx][yy];
+                                parent[i][j][xx][yy] = {i,j+1};
+                            }
+                        }
+                    }
+                    else if(M[i][j] == 0 && xx > 0){
+                        if((dp[i][j+1][xx-1][yy] != IINF) && (parent[i][j+1][xx-1][yy].first != i || parent[i][j+1][xx-1][yy].second != j)){
+                            if(M[i][j] + dp[i][j+1][xx-1][yy] < dp[i][j][xx][yy]){
+                                dp[i][j][xx][yy] = M[i][j] + dp[i][j+1][xx-1][yy];
+                                parent[i][j][xx][yy] = {i,j+1};
+                            }
+                        }
+                    }
+                    else if(M[i][j] < 0 && yy > 0){
+                        if((dp[i][j+1][xx][yy-1] != IINF) && (parent[i][j+1][xx][yy-1].first != i || parent[i][j+1][xx][yy-1].second != j)){
+                            if(M[i][j] + dp[i][j+1][xx][yy-1] < dp[i][j][xx][yy]){
+                                dp[i][j][xx][yy] = M[i][j] + dp[i][j+1][xx][yy-1];
+                                parent[i][j][xx][yy] = {i,j+1};
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
-
-// void sol_dp(int &n, int &m, int &x, int &y, imatrix &M, imatrix4d &dp){
-//     dp = imatrix4d(n,vector<vector<vector<int>>>(m,imatrix(x,vector<int>(y,IINF))));
-//     imatrix4d visited(n,vector<vector<vector<int>>>(m,imatrix(x,vector<int>(y,0))));
-
-//     if(M[0][0] > 0 || (M[0][0] = 0 && x > 0) || (M[0][0] < 0 && y > 0)){
-//         dp[0][0][x][y] = M[0][0];
-//         visited[0][0][x][y] = 1;
-//     }
-//     else{
-//         return;
-//     }
-
-//     for(int i = 0; i < n; i++){
-//         for(int j = 0; j < m; j++){
-//             if(j > 0){
-//                 int left_neigh_cost = 
-//             }
-//         }
-//     }
-// }
-
 
 int main(){
-    //std::ios::sync_with_stdio(0);cin.tie(0);
+    std::ios::sync_with_stdio(0);cin.tie(0);
 
     int n,m,x,y;
 
@@ -65,9 +138,16 @@ int main(){
                 cin >> M[i][j];
             }
         }
-        print_matrix<int>(M);
 
-        imatrix dp;
+        imatrix4d dp;
+
+        sol_dp(n,m,x,y,M,dp);
+        if(dp[n-1][m-1][x][y] == IINF){
+            cout << "Impossivel" << endl;
+        }
+        else{
+            cout << dp[n-1][m-1][x][y] << endl;
+        }
 
     }
 
