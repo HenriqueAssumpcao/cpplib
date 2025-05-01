5b5c838f0e02c3942052b46faa9fbde352cd2dd9
HenriqueAssumpcao <henriquesas2010@gmail.com>
2025-05-01 20:13:26 -0300

    ...

--- problems/cses/download_speed.cpp
+++ problems/cses/download_speed.cpp
@@ -1,94 +1,115 @@
 #include<bits/stdc++.h>
- 
+
 using namespace std;
- 
+
 #define endl '\n'
- 
+
 #define LLINF LLONG_MAX/2
 #define IINF INT_MAX/2
- 
+
 typedef long long ll;
- 
-struct edge{
-    int to;
-    ll cap,flow;
-    edge *opp;
- 
-    edge(int to, ll cap, ll flow, edge *opposite):to(to),cap(cap),flow(flow),opp(opp){};
+typedef vector<list<int>> graph; // unweighted graph
+typedef vector<vector<int>> imatrix;
+typedef vector<vector<ll>> llmatrix;
+
+// Edmonds Karp 
+
+struct flow_edge{
+    int u,v; // Endpoits of directed edge
+    ll cap,flow = 0;
+    bool in_g; // 1 if edge belongs to the original graph, 0 if it does not
+    flow_edge(int _u,int _v, ll _cap, bool _in_g):u(_u),v(_v),cap(_cap),in_g(_in_g){};
+
 };
- 
-typedef vector<vector<edge*>> flow_graph;
- 
-ll find_aug_path(int s, int t,int &n, flow_graph &g, vector<pair<int,edge*>> &parent){
-    parent = vector<pair<int,edge*>>(n,{-1,nullptr});
- 
-    vector<bool> is_visited(n,0);
-    is_visited[s] = 1;
- 
+
+ll find_aug_path(int s, int t, int &n, graph &g, vector<flow_edge> &edges, vector<int> &parent){
+    parent = vector<int>(n,-1);
+    vector<bool> visited(n,0);
+    visited[s] = 1;
+
     queue<pair<int,ll>> q;
- 
     q.push({s,LLINF});
- 
+
     while(q.size()){
-        auto [u,curr_flow] = q.front();
+        auto [u,flow] = q.front();
         q.pop();
-        for(edge *e : g[u]){
-            if(is_visited[e->to] == 0 && e->cap > 0){
-                is_visited[e->to] = 1;
-                parent[e->to] = {u,e};
-                curr_flow = min(curr_flow,e->cap);
-                if(e->to == t){
-                    return curr_flow;
+
+        for(int i : g[u]){
+            if(visited[edges[i].v] == 0 && edges[i].cap > 0){
+                visited[edges[i].v] = 1;
+                parent[edges[i].v] = i;
+                flow = min(flow,edges[i].cap);
+                if(edges[i].v == t){
+                    return flow;
                 }
-                q.push({e->to,curr_flow});
+                q.push({edges[i].v,flow});
             }
         }
     }
     return 0;
 }
- 
-ll edmonds_karp_maxflow(int s, int t, int &n, flow_graph &g){
+
+ll edmonds_karp_maxflow(int s, int t, int &n, graph &g, vector<flow_edge> &edges){
     ll max_flow = 0;
-    vector<pair<int,edge*>> parent;
- 
-    ll flow;
-    while(flow = find_aug_path(s,t,n,g,parent)){
-        max_flow += flow;
+    vector<int> parent;
+
+    ll curr_flow;
+    while(curr_flow = find_aug_path(s,t,n,g,edges,parent)){
+        max_flow += curr_flow;
         int temp = t;
         while(temp != s){
-            int prev = parent[temp].first;
-            edge *curr_edge = parent[temp].second;
-            curr_edge->cap -= flow;
-            curr_edge->flow += flow;
-            curr_edge->opp->cap += flow;
-            curr_edge->opp->flow -= flow;
+            int prev = edges[parent[temp]].u;
+            int op_edge_id = (edges[parent[temp]].in_g == 1) ? (parent[temp]+1) : (parent[temp]-1);
+
+            edges[parent[temp]].cap -= curr_flow;
+            edges[parent[temp]].flow += curr_flow;
+            edges[op_edge_id].cap += curr_flow;
+            edges[op_edge_id].flow -= curr_flow;
             temp = prev;
         }
     }
- 
+
     return max_flow;
 }
- 
- 
+
+void min_cut(int s, int &n, graph &g, vector<flow_edge> &edges, vector<int> &is_in_cut){
+    is_in_cut = vector<int>(n,0);
+    is_in_cut[s] = 1;
+
+    queue<int> q;
+    q.push(s);
+
+    while(q.size()){
+        int u = q.front();
+        q.pop();
+        for(int i : g[u]){
+            if(is_in_cut[edges[i].v] == 0 && edges[i].cap > 0){
+                is_in_cut[edges[i].v] = 1;
+                q.push(edges[i].v);
+            }
+        }
+    }
+}
+
 int main(){
- 
+
     int n,m;
- 
     cin >> n >> m;
- 
-    flow_graph g(n);
+
+    vector<flow_edge> edges;
+    graph g(n);
+    int edge_id = 0;
     int u,v;
     ll c;
     for(int i = 0; i < m; i++){
         cin >> u >> v >> c;
         u--;v--;
-        edge *e1 = new edge(v,c,0,nullptr),*e2 = new edge(u,0,0,nullptr);
-        e1->opp = e2;e2->opp = e1;
-        g[u].push_back(e1);
-        g[v].push_back(e2);
+        edges.push_back(flow_edge(u,v,c,1));
+        edges.push_back(flow_edge(v,u,0,0));
+        g[u].push_back(edge_id);
+        g[v].push_back(edge_id+1);
+        edge_id += 2;
     }
- 
-    cout << edmonds_karp_maxflow(0,n-1,n,g) << endl;
- 
-    return 0;
+
+    cout << edmonds_karp_maxflow(0,n-1,n,g,edges) << endl;
 }
